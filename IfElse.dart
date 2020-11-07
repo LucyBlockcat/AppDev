d3e475f4d8ced1742cfbe5cbf419711abce5ba6c
LucyBlockcat <74011051+LucyBlockcat@users.noreply.github.com>
2020-11-07 15:46:21 +0100

    Create IfElse.dart

---
+++ IfElse.dart
@@ -0,0 +1,9 @@
+void main(){
+  int zahlengrenze = 7;
+  if (zahlengrenze<10){
+    print("$zahlengrenze ist kleiner als 10");
+  }
+  else{
+    print("$zahlengrenze ist größer oder gleich 10 ");
+  }
+}
