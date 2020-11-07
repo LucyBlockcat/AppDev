75f71c54fc14a68833b9f77fe330b38db9be5b58
LucyBlockcat <74011051+LucyBlockcat@users.noreply.github.com>
2020-11-07 15:04:53 +0100

    Create Strings.dart

---
+++ Strings.dart
@@ -0,0 +1,12 @@
+void main() {
+  String meinName = "Josi";
+  print("Hallo, mein Name ist $meinName");
+  
+  double groesse= 1.72;
+  
+  String casting = groesse.toString();
+  print("Ich bin $groesse Meter groß");
+  
+  int anzahlDerBuchstaben = meinName.length;
+  print("Mein Name ist $anzahlDerBuchstaben Buchstaben lang");
+  }
